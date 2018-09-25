e5e17cbde2f6f8644d29d2a53e0885a0a4f84ce8
Kyle <kyle.karpack@net-inspect.com>
2018-09-25 10:44:20 -0700

    Initial project setup commit
    Added generated angular-cli app and some basic code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import * as GpxParse from "gpx-parse/dist/gpx-parse-browser.js";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+	title = "app";
+
+	constructor() {
+		console.warn(GpxParse);
+
+	}
+
+	public filesAdded($event: any): void {
+		console.warn($event);
+
+		const reader = new FileReader();
+		reader.onload = (data) => {
+			console.log(data)
+			GpxParse.parseGpx(data.target.result, (error, result) => {
+				console.warn(result)
+			});
+		};
+
+		for (let file of $event.target.files) {
+			reader.readAsText(file);
+		}
+
+	}
+
 }
