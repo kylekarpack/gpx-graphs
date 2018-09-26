a7a8ac8d6bab2fb8cb9885dcc075d364645d5481
Kyle <kyle.karpack@net-inspect.com>
2018-09-26 07:51:05 -0700

    Added basic distance calculations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from "@angular/core";
 import * as GpxParse from "gpx-parse/dist/gpx-parse-browser.js";
+import * as toGeoJson from "@mapbox/togeojson";
+import length from "@turf/length";
 
 @Component({
 	selector: "app-root",
@@ -7,27 +9,58 @@ import * as GpxParse from "gpx-parse/dist/gpx-parse-browser.js";
 	styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-	title = "app";
 
-	constructor() {
-		console.warn(GpxParse);
+	constructor() {	}
+
+	public options: any = {
+		units: "miles"
+	};
+
+	// Simple tracker for stats
+	public stats = {
+		distance: 0,
+		elevation: 0
+	};
+
+	public filesAdded($event: any): void {
+
+		const results = [];
+		
+
+		for (let file of $event.target.files) {
+
+			const reader = new FileReader();
+
+			reader.onload = (data: any) => {
+				// ToDo: re-implement this to add back time as a factor
+				// GpxParse.parseGpx(data.target.result, (error, result) => {
+				// 	if (error) {
+				// 		alert("Something went wrong!");
+				// 		console.error(error);
+				// 	}
+				// 	});
+
+				const xml = new DOMParser().parseFromString(data.target.result, "text/xml"),
+					geoJson = toGeoJson.gpx(xml);
+
+				results.push(geoJson);
+
+				// Process if all complete
+				if (results.length >= $event.target.files.length) {
+					this.process(results);
+				}
+	
+
+			};
+
+			reader.readAsText(file);
+		}
 
 	}
 
-	public filesAdded($event: any): void {
-		console.warn($event);
-
-		const reader = new FileReader();
-		reader.onload = (data) => {
-			console.log(data)
-			GpxParse.parseGpx(data.target.result, (error, result) => {
-				console.warn(result)
-			});
-		};
-
-		for (let file of $event.target.files) {
-			reader.readAsText(file);
-		}
+	private process(results: any[]): void {
+		// Get a sum of the distances
+		this.stats.distance = results.map(el => length(el, this.options)).reduce((a,b) => a + b, 0);
 
 	}
 
