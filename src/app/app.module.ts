cfda81a038aced07643c0800bfe0045aaaf09a7b
Kyle <kyle.karpack@net-inspect.com>
2019-04-24 06:18:17 -0700

    Cleaned up tests a little

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DxDataGridComponent, DxDataGridModule, DxCheckBoxModule, DxSparklineModule, DxTemplateModule } from 'devextreme-angular';
-  import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
-  
+import { DxDataGridModule, DxSparklineModule, DxTemplateModule } from 'devextreme-angular';
+import { NgxMapboxGLModule } from 'ngx-mapbox-gl';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    DxDataGridModule,
-    DxSparklineModule,
-    DxTemplateModule,
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        DxDataGridModule,
+        DxSparklineModule,
+        DxTemplateModule,
 
-    NgxMapboxGLModule.withConfig({
-      accessToken: 'pk.eyJ1Ijoid3lra3NzIiwiYSI6ImNqMjR6aTdmdzAwNHMzMnBvbjBucjlqNm8ifQ.6GjGpofWBVaIuSnhdXQb5w'
-    })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+        NgxMapboxGLModule.withConfig({
+            accessToken: 'pk.eyJ1Ijoid3lra3NzIiwiYSI6ImNqMjR6aTdmdzAwNHMzMnBvbjBucjlqNm8ifQ.6GjGpofWBVaIuSnhdXQb5w'
+        })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
